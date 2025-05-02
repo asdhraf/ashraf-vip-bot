e005962d348dd8ea1f32578ed055427832025757
asdhraf <ashrafaljohni1@gmail.com>
2025-05-02 14:45:02 +0300

    حذف الملف

--- send_test_message.py
+++ send_test_message.py
@@ -1,17 +1,1 @@
-import requests
 
-BOT_TOKEN = '7698515759:AAGJzxXN4t5yPUzSHHr4nv4xSzqUM5E1FHs'
-CHAT_ID = '@ashraf_1m_bot'
-
-def send_message():
-    message = "✅ تم إرسال هذه الرسالة بنجاح من البوت للتجربة."
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    payload = {
-        'chat_id': CHAT_ID,
-        'text': message,
-        'parse_mode': 'HTML'
-    }
-    response = requests.post(url, data=payload)
-    print(response.text)
-
-send_message()
